1c4990d466c12022daaf324fbbbad1e22e883dd0
Gaurav Kumar Rana <gaurav.rana@freescale.com>
2013-11-15 15:52:59 +0530

    Added legacy boot feature
    	-A legacy_flag will be set when legacy boot image is created.
    	-And only cf_hdr_legacy and cfwords will be populated.
    	-Added sample image format for legacy_cfsign in input_files.
    
    Signed-off-by: Gaurav Kumar Rana <gaurav.rana@freescale.com>
    Change-Id: I4577a5ddc6d81b51dccc05e087bd06e05951c6ad
    Reviewed-on: http://git.am.freescale.net:8181/6560
    Reviewed-by: Aneesh Bansal <aneesh.bansal@freescale.com>
    Reviewed-by: Ruchika Gupta <ruchika.gupta@freescale.com>
    Tested-by: Ruchika Gupta <ruchika.gupta@freescale.com>

--- uni_cfsign.c
+++ uni_cfsign.c
@@ -28,12 +28,12 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 /*
- *    "This product includes software developed by the OpenSSL Project
- *    for use in the OpenSSL Toolkit (http://www.openssl.org/)"
+ *"This product includes software developed by the OpenSSL Project
+ * for use in the OpenSSL Toolkit (http://www.openssl.org/)"
  */
 /*
- *    "This product includes cryptographic software written by
- *     Eric Young (eay@cryptsoft.com)"
+ *"This product includes cryptographic software written by
+ * Eric Young (eay@cryptsoft.com)"
  */
 
 #define OPENSSL_NO_KRB5
@@ -48,6 +48,8 @@
 #include "common.h"
 #include "uni_cfsign.h"
 
+#define IOBLOCK 128		/* I/O block size used for hashing operations */
+
 FILE *fhdr;
 u32 targetid, esbc_hdr, esbc_hdr_simg;
 struct size_format size;
@@ -58,6 +60,44 @@ struct global gd;
 u32 words[2048];
 u32 word_pairs;
 int word_count;
+int legacy_flag;
+
+char *image_name;
+u32 code_len;
+u32 src_addr;
+u32 dst_addr;
+u32 entry_point;
+
+
+/* return the size of the give file */
+static int get_size(const char *c)
+{
+	FILE *fp;
+	unsigned char buf[IOBLOCK];
+	size_t bytes = 0;
+
+	if (c == NULL)
+		return 0;
+
+	fp = fopen(c, "rb");
+	if (fp == NULL) {
+		fprintf(stderr, "Error in opening the file: %s\n", c);
+		exit(0);
+	}
+
+	while (!feof(fp)) {
+		/* read some data */
+		bytes += fread(buf, 1, IOBLOCK, fp);
+		if (ferror(fp)) {
+			fprintf(stderr, "Error in reading file\n");
+			return -1;
+		} else if (feof(fp) && (bytes == 0))
+			break;
+	}
+
+	fclose(fp);
+	return bytes;
+}
 
 int check_group(char *platform)
 {
@@ -138,7 +178,7 @@ void parse_file(char *file_name)
 		}
 
 	} else if ((file_field.count == 0) || (file_field.count == -1)) {
-		printf("Error.Platform not found in input file. Refer usage\n");
+		printf("Error.Platform not found in input file.Refer usage\n");
 		exit(1);
 	}
 	/* Parse from input file */
@@ -156,21 +196,73 @@ void parse_file(char *file_name)
 	}
 
 
+	/* Parse Images from input file */
+
+		find_value_from_file("IMAGE_LOC", fp);
+		if (((gd.group == 1) || (gd.group == 2)) &&
+			(file_field.count != 3)
+			&& (file_field.count != -1)) {
+
+			printf("Error. Invalid Usage. Please check IMAGE in"
+				" input file. Refer usage ::\n");
+			exit(1);
+		}
+
+		if (file_field.count == 3) {
+			image_name =
+				malloc(strlen(file_field.value[0]));
+			strcpy(image_name, file_field.value[0]);
+			src_addr =
+				strtoul(file_field.value[1], 0, 16);
+
+			if (((gd.group == 1) || (gd.group == 2))) {
+				dst_addr =
+					strtoul(file_field.value[2], 0, 16);
+			}
+
+		}
+#ifdef DEBUG
+		printf("%s ", gd.entries[i].name);
+		printf("%x ", gd.entries[i].addr);
+#endif
+
+	/* get the length of image file */
+	code_len = get_size(image_name);
+
+
+	/* if it is legacy boot than flag is set otherwise it remains unset*/
+	if (code_len && image_name != NULL)
+		legacy_flag = 1;
+
+
+	/* Parse Entry Point from input file */
+		find_value_from_file("ENTRY_POINT", fp);
+		if (file_field.count == 1)
+			entry_point = strtoul(file_field.value[0], 0, 16);
+		else if (legacy_flag != 0) {
+			printf("Error. Invalid Usage. Please check ENTRY_POINT"
+				"in input file. Refer usage ::\n");
+			exit(1);
+		}
+
+
+
 	find_value_from_file("ESBC_HDRADDR", fp);
 	if (file_field.count == 1) {
 		esbc_hdr = strtoul(file_field.value[0], 0, 16);
 
-	} else if ((file_field.count == 0) || (file_field.count == -1)) {
-		printf
-		    ("Error.Primary ESBC Header not found in input file. Refer usage\n");
+	} else if (((file_field.count == 0) || (file_field.count == -1))
+			&& (legacy_flag == 0)) {
+		printf("Error.Primary ESBC Header not found in"
+			" input file. Refer usage\n");
 		exit(1);
 	}
 
 	find_value_from_file("ESBC_HDRADDR_SEC_IMAGE", fp);
 	if (file_field.count == 1) {
 		if (gd.group == 1) {
-			printf
-			    ("Error.Secondaery Image not required for this platform. Refer usage\n");
+			printf("Error.Secondaery Image not required for"
+				" this platform. Refer usage\n");
 			exit(1);
 		} else {
 			esbc_hdr_simg = strtoul(file_field.value[0], 0, 16);
@@ -178,16 +270,16 @@ void parse_file(char *file_name)
 
 	} else if (((file_field.count == 0) || (file_field.count == -1))
 		   && (gd.group == 2)) {
-		printf
-		    ("Error.Secondary Image HDR Addr not found in input file. Refer usage\n");
+		printf("Error.Secondary Image HDR Addr not found in"
+			" input file. Refer usage\n");
 		exit(1);
 	}
 	/* Parse Key Info from input file */
 	find_value_from_file("KEY_SELECT", fp);
 	if (file_field.count == 1) {
 		if (gd.group == 1) {
-			printf
-			    ("Error.Key Select not required for this platform. Refer usage\n");
+			printf("Error.Key Select not required for this"
+				" platform. Refer usage\n");
 			exit(1);
 		} else {
 
@@ -210,6 +302,8 @@ void parse_file(char *file_name)
 			i++;
 		}
 	}
+
+
 
 	find_cfw_from_file("CF_WORD", fp);
 
@@ -252,7 +346,7 @@ int open_priv_file(void)
 int main(int argc, char **argv)
 {
 	int ret;
-	int i, j, n;
+	int i, j, n ;
 	u8 *tmp;
 	u8 *sign;
 	uint32_t sign_len;
@@ -283,6 +377,7 @@ int main(int argc, char **argv)
 	strcpy(input_pri_key.value[0], PRI_KEY_FILE);
 	gd.hdrfile = malloc(strlen(HDR_FILE));
 	strcpy(gd.hdrfile, HDR_FILE);
+	image_name = NULL;
 
 	printf("\n");
 	printf("============================================================"
@@ -308,10 +403,6 @@ int main(int argc, char **argv)
 		exit(1);
 
 	}
-	printf("\n");
-	printf("######################################################\n");
-	printf("## Generating CFHeader composite image (pkey & sign)##\n");
-	printf("######################################################\n");
 	i = 0;
 	if ((input_pri_key.count > 1) && (gd.group == 1)) {
 		printf("Error. More than 1 key is not required"
@@ -326,24 +417,31 @@ int main(int argc, char **argv)
 		gd.num_srk_entries = input_pri_key.count;
 	}
 
-	ret = open_priv_file();
-	if (ret < 0)
-		exit(1);
+
+
+	if (legacy_flag == 0) {
+		ret = open_priv_file();
+		if (ret < 0)
+			exit(1);
+
 #ifdef DEBUG
 	printf("\nNo. of keys - %d", gd.num_srk_entries);
 	printf("\nGroup - %d", gd.group);
 #endif
-	if (gd.srk_table_flag == 0) {
-		key_len = size.sign_len = RSA_size(gd.srk[0]);
-		size.key_table = 2 * key_len;
-	} else {
-		i = 0;
-		while (i != gd.num_srk_entries) {
-			gd.key_table[i].key_len = RSA_size(gd.srk[i]);
-			i++;
+
+		if (gd.srk_table_flag == 0) {
+			key_len = size.sign_len = RSA_size(gd.srk[0]);
+			size.key_table = 2 * key_len;
+		} else {
+			i = 0;
+			while (i != gd.num_srk_entries) {
+				gd.key_table[i].key_len = RSA_size(gd.srk[i]);
+				i++;
+			}
+			size.sign_len = gd.key_table[gd.srk_sel - 1].key_len;
+			size.key_table = gd.num_srk_entries *
+					sizeof(struct srk_table);
 		}
-		size.sign_len = gd.key_table[gd.srk_sel - 1].key_len;
-		size.key_table = gd.num_srk_entries * sizeof(struct srk_table);
 	}
 
 	word_pairs = word_count / 2;
@@ -363,9 +461,15 @@ int main(int argc, char **argv)
 	    (factor + 2) * 512 - (size.hdr_legacy + size.cfw + size.hdr_secure +
 				  0x40);
 	size.padd2 = 0x1200 - size.key_table;
-	buf_len = 0x40 + size.hdr_legacy + size.cfw +
-	    size.hdr_secure + size.key_table + size.sign_len + size.padd1 +
-	    size.padd2;
+
+	/* cfheader size would be different in secure boot and legacy boot. */
+	if (legacy_flag == 0)
+		buf_len = 0x40 + size.hdr_legacy + size.cfw + size.hdr_secure +
+			size.key_table + size.sign_len + size.padd1 +
+			size.padd2;
+	else
+		buf_len = 0x40 + size.hdr_legacy + size.cfw;
+
 
 	buf = malloc(buf_len);
 	if (buf == NULL) {
@@ -390,133 +494,154 @@ int main(int argc, char **argv)
 	cwds = (u8 *) cfl + size.hdr_legacy;
 	memcpy(cwds, (u8 *) words, size.cfw);
 
-	/* Secure boot additions to legacy header */
-	cfs = (struct cf_hdr_secure *)((u8 *) cfl + size.hdr_legacy + size.cfw);
-
-	cfs->ehdrloc = htonl(esbc_hdr);
-	cfs->esbc_target_id = htonl(targetid);
-	cfs->psign_off = htonl(sign_offset);
-	cfs->sign_len = htonl(size.sign_len);
-	if (gd.srk_table_flag == 0) {
-		cfs->pkey_off = htonl(key_offset);
-		cfs->key_len = htonl(2 * key_len);
-	} else {
-		temp = 0;
-		temp = temp | gd.srk_table_flag;
-		temp = temp << 12;
-		temp = temp | gd.srk_sel;
-		temp = htons(temp);
-		memcpy((u8 *) & (cfs->len_kr), &temp, 2);
-		cfs->len_kr.num_srk_entries = htons(gd.num_srk_entries);
-		cfs->srk_table_offset = htonl(key_offset);
+	/* store code_len, src_addr, dst_addr, entry_point in case of legacy
+		boot and if image_name is null all fields will be zeroised */
+	if (legacy_flag == 1) {
+		cfl->code_len = htonl(code_len);
+		cfl->src_addr = htonl(src_addr);
+		cfl->dst_addr = htonl(dst_addr);
+		cfl->entry_point = htonl(entry_point);
 	}
-	if (gd.group == 2) {
-		/*Secobdary Image header Location */
-		cfs->ehdrloc_simg = htonl(esbc_hdr_simg);
-	}
-	/* Copy PublicKey */
-	/* copy N and E */
 
-	if (gd.srk_table_flag == 0) {
+	if (legacy_flag == 0) {
+		/* Secure boot additions to legacy header */
+		cfs = (struct cf_hdr_secure *)((u8 *) cfl + size.hdr_legacy +
+			size.cfw);
+
+		cfs->ehdrloc = htonl(esbc_hdr);
+		cfs->esbc_target_id = htonl(targetid);
+		cfs->psign_off = htonl(sign_offset);
+		cfs->sign_len = htonl(size.sign_len);
+		if (gd.srk_table_flag == 0) {
+			cfs->pkey_off = htonl(key_offset);
+			cfs->key_len = htonl(2 * key_len);
+		} else {
+			temp = 0;
+			temp = temp | gd.srk_table_flag;
+			temp = temp << 12;
+			temp = temp | gd.srk_sel;
+			temp = htons(temp);
+			memcpy((u8 *) &(cfs->len_kr), &temp, 2);
+			cfs->len_kr.num_srk_entries =
+				htons(gd.num_srk_entries);
+			cfs->srk_table_offset = htonl(key_offset);
+		}
+		if (gd.group == 2) {
+			/*Secobdary Image header Location */
+			cfs->ehdrloc_simg = htonl(esbc_hdr_simg);
+		}
+		/* Copy PublicKey */
 		/* copy N and E */
 
-		key = (u8 *) cfl + key_offset;
-		/* Copy N component */
-		tmp = (unsigned char *)(((BIGNUM *) gd.srk[0]->n)->d);
-		for (j = key_len - 1, i = 0;
-		     i < ((BIGNUM *) gd.srk[0]->n)->top * sizeof(BIGNUM *);
-		     i++, j--)
-			key[j] = tmp[i];
-
-		/* Copy E component */
-		key = (u8 *) cfl + key_offset + key_len;
-		tmp = (unsigned char *)(((BIGNUM *) gd.srk[0]->e)->d);
-		for (j = key_len - 1, i = 0;
-		     i < ((BIGNUM *) gd.srk[0]->e)->top * sizeof(BIGNUM *);
-		     i++, j--)
-			key[j] = tmp[i];
-
-	} else {
-		/* SRK table */
-		n = 0;
-		while (n != gd.num_srk_entries) {
+		if (gd.srk_table_flag == 0) {
 			/* copy N and E */
-			key =
-			    (u8 *) cfl + key_offset +
-			    (n) * (sizeof(struct srk_table));
 
-			/* Copy length */
-			total_key_len = htonl(2 * gd.key_table[n].key_len);
-			memcpy(key, &total_key_len, sizeof(u32));
-			key = key + sizeof(u32);
-
+			key = (u8 *) cfl + key_offset;
 			/* Copy N component */
-			tmp = (unsigned char *)(((BIGNUM *) gd.srk[n]->n)->d);
-			for (j = gd.key_table[n].key_len - 1, i = 0;
-			     i <
-			     ((BIGNUM *) gd.srk[n]->n)->top * sizeof(BIGNUM *);
-			     i++, j--)
-				key[j] = tmp[i];
+			tmp = (unsigned char *)(((BIGNUM *) gd.srk[0]->n)->d);
+			for (j = key_len - 1, i = 0;
+				i < ((BIGNUM *) gd.srk[0]->n)->top *
+				sizeof(BIGNUM *); i++, j--)
+					key[j] = tmp[i];
 
 			/* Copy E component */
-			key =
-			    (u8 *) cfl + key_offset +
-			    (n) * (sizeof(struct srk_table)) + sizeof(u32) +
-			    gd.key_table[n].key_len;
-			tmp = (unsigned char *)(((BIGNUM *) gd.srk[n]->e)->d);
-			for (j = gd.key_table[n].key_len - 1, i = 0;
-			     i <
-			     ((BIGNUM *) gd.srk[n]->e)->top * sizeof(BIGNUM *);
-			     i++, j--)
-				key[j] = tmp[i];
+			key = (u8 *) cfl + key_offset + key_len;
+			tmp = (unsigned char *)(((BIGNUM *) gd.srk[0]->e)->d);
+			for (j = key_len - 1, i = 0;
+				i < ((BIGNUM *) gd.srk[0]->e)->top *
+				sizeof(BIGNUM *); i++, j--)
+					key[j] = tmp[i];
 
-			key =
-			    (u8 *) cfl + key_offset +
-			    (n) * (sizeof(struct srk_table));
-			memcpy(gd.key_table[n].pkey,
-			       (u8 *) cfl + key_offset +
-			       n * sizeof(struct srk_table) + 4,
-			       2 * gd.key_table[n].key_len);
-			/*Update for all the keys present in the Key table */
-			n++;
+		} else {
+			/* SRK table */
+			n = 0;
+			while (n != gd.num_srk_entries) {
+				/* copy N and E */
+				key =
+				    (u8 *) cfl + key_offset +
+				    (n) * (sizeof(struct srk_table));
+
+				/* Copy length */
+				total_key_len =
+					htonl(2 * gd.key_table[n].key_len);
+				memcpy(key, &total_key_len, sizeof(u32));
+				key = key + sizeof(u32);
+
+				/* Copy N component */
+				tmp = (unsigned char *)
+					(((BIGNUM *) gd.srk[n]->n)->d);
+				for (j = gd.key_table[n].key_len - 1, i = 0;
+					i < ((BIGNUM *) gd.srk[n]->n)->top *
+					sizeof(BIGNUM *); i++, j--)
+						key[j] = tmp[i];
+
+				/* Copy E component */
+				key =
+					(u8 *) cfl + key_offset +
+					(n) * (sizeof(struct srk_table)) +
+					sizeof(u32) + gd.key_table[n].key_len;
+				tmp = (unsigned char *)
+					(((BIGNUM *) gd.srk[n]->e)->d);
+				for (j = gd.key_table[n].key_len - 1, i = 0;
+					i < ((BIGNUM *) gd.srk[n]->e)->top *
+					sizeof(BIGNUM *); i++, j--)
+						key[j] = tmp[i];
+
+				key =
+				    (u8 *) cfl + key_offset +
+				    (n) * (sizeof(struct srk_table));
+				memcpy(gd.key_table[n].pkey,
+				       (u8 *) cfl + key_offset +
+				       n * sizeof(struct srk_table) + 4,
+				       2 * gd.key_table[n].key_len);
+				/*Update for all the keys present in the
+					Key table */
+				n++;
+			}
+
 		}
 
-	}
+		SHA256_CTX ctx;
+		SHA256_Init(&ctx);
+		SHA256_Update(&ctx, (u8 *) cfl + key_offset, size.key_table);
+		SHA256_Final(hash, &ctx);
 
-	SHA256_CTX ctx;
-	SHA256_Init(&ctx);
-	SHA256_Update(&ctx, (u8 *) cfl + key_offset, size.key_table);
-	SHA256_Final(hash, &ctx);
+		printf("\n");
+		printf("######################################################\n");
+		printf("## Generating CFHeader composite image (pkey & sign)##\n");
+		printf("######################################################\n");
 
-	printf("\n");
-	printf("pkey Hash :");
-	for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
-		printf("%02x", hash[i]);
+		printf("\n");
+		printf("pkey Hash :");
+		for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
+			printf("%02x", hash[i]);
 
-	/* hash-->hdr+key + */
-	SHA256_Init(&ctx);
-	SHA256_Update(&ctx, cfl, size.hdr_legacy);
+		/* hash-->hdr+key + */
 
-	SHA256_Update(&ctx, cwds, size.cfw);
-	SHA256_Update(&ctx, cfs, size.hdr_secure);
-	SHA256_Update(&ctx, (u8 *) cfl + key_offset, size.key_table);
-	SHA256_Final(hash, &ctx);
+		SHA256_Init(&ctx);
+		SHA256_Update(&ctx, cfl, size.hdr_legacy);
+		SHA256_Update(&ctx, cwds, size.cfw);
+		SHA256_Update(&ctx, cfs, size.hdr_secure);
+		SHA256_Update(&ctx, (u8 *) cfl + key_offset, size.key_table);
+		SHA256_Final(hash, &ctx);
 
-	printf("\n");
+		printf("\n");
 #ifdef DEBUG
-	printf("Hash :");
-	for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
-		printf("%02x", hash[i]);
-	printf("\n");
+		printf("Hash :");
+		for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
+			printf("%02x", hash[i]);
+			printf("\n");
 #endif
-	/* copy Sign */
-	sign = (u8 *) (cfl) + sign_offset;
-	if (RSA_sign
-	    (NID_sha256, hash, SHA256_DIGEST_LENGTH, sign, &sign_len,
-	     gd.srk[gd.srk_sel - 1])
-	    != 1) {
-		fprintf(stderr, "Error signing the data\n");
-		return -1;
+
+		/* copy Sign */
+		sign = (u8 *) (cfl) + sign_offset;
+		if (RSA_sign
+			(NID_sha256, hash, SHA256_DIGEST_LENGTH, sign,
+			&sign_len, gd.srk[gd.srk_sel - 1]) != 1) {
+			fprintf(stderr, "Error signing the data\n");
+			return -1;
+		}
+
 	}
 
 	fhdr = fopen(gd.hdrfile, "wb");
@@ -525,6 +650,7 @@ int main(int argc, char **argv)
 		return -1;
 	}
 	printf("\n");
+
 #ifdef DEBUG
 	printf("Sign :");
 	for (i = 0; i < sign_len; i++)
@@ -535,14 +661,19 @@ int main(int argc, char **argv)
 	/* Concatinating Header, Sign */
 	fwrite((unsigned char *)buf, 1, buf_len, fhdr);
 	printf("\nHeader File Created : %s\n", gd.hdrfile);
-
+	if (legacy_flag == 0 && buf_len > esbc_hdr)
+		printf("\nError cfheader overlaps esbc header\n"
+			"buf_len : %xESBC HEADER %x\n", buf_len, esbc_hdr);
 	/* clean up */
 	free(buf);
 	free(gd.hdrfile);
-	for (i = 0; i < gd.num_srk_entries; i++) {
-		free(input_pri_key.value[i]);
-		fclose(gd.fsrk_pri[i]);
-		RSA_free(gd.srk[i]);
+
+	if (legacy_flag == 0) {
+		for (i = 0; i < gd.num_srk_entries; i++) {
+			free(input_pri_key.value[i]);
+			fclose(gd.fsrk_pri[i]);
+			RSA_free(gd.srk[i]);
+		}
 	}
 	fclose(fhdr);
 	return 0;
