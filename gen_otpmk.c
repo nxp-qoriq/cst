8023fa14f723049656bdc425e0b54e5bf049084e
Gaurav Kumar Rana <gaurav.rana@freescale.com>
2014-09-16 15:35:11 +0530

    Modify usage of gen_otpmk utility.
    - In Trust 1.x, the key is 255-0, and OTPMK 0 holds bits 31-0.
    - In Trust 2.0, the key is 255-0, and OTPMK 0 holds bits 255-224, rather than 31-0.
    - Fix the issue with gen_rand_string() function.
    
    Signed-off-by: Gaurav Kumar Rana <gaurav.rana@freescale.com>
    Change-Id: I06ea66882beba7061f2a060d3637208d66c23301
    Reviewed-on: http://git.am.freescale.net:8181/13061
    Reviewed-by: Ruchika Gupta <ruchika.gupta@freescale.com>
    Tested-by: Ruchika Gupta <ruchika.gupta@freescale.com>
    (cherry picked from commit 78dd65293758ecbc3f0242e71d384f5fc1d3ca0b)
    Reviewed-on: http://git.am.freescale.net:8181/18819

--- gen_otpmk.c
+++ gen_otpmk.c
@@ -44,12 +44,18 @@
 typedef unsigned char u8;
 
 u8 otpmk_hex[OTPMK_SIZE_BYTES];
+int trust_arch;
+
+/* There is a change in SFP word ordering from Trust 1.x to Trust 2.0
+ * In Trust 2.0 devices the key is 255-0, and OTPMK 0 holds bits 255-224,
+ * rather than 31-0, while in Trust 1.x devices OTPMK 0 holds bits 31-0.
+ * */
 
 /* Generate the Hamming code bits for the 256 bits stored in number.
  * The values at the locations of the code bits are ignored and is
  * overwritten with the generated values.
  */
-void print_otpmk()
+void print_otpmk_trust1()
 {
 	int i;
 	printf("\n NAME    |     BITS     |    VALUE  ");
@@ -63,6 +69,30 @@ void print_otpmk()
 		       (OTPMK_REG_NO - 1 - i / 4) * OTPMK_SIZE_BYTES,
 		       otpmk_hex[i], otpmk_hex[i + 1], otpmk_hex[i + 2],
 		       otpmk_hex[i + 3]);
+	}
+
+}
+
+/* Generate the Hamming code bits for the 256 bits stored in number.
+ * The values at the locations of the code bits are ignored and is
+ * overwritten with the generated values.
+ */
+void print_otpmk_trust2()
+{
+	int i;
+	int j = 0;
+	printf("\n NAME    |     BITS     |    VALUE  ");
+	printf("\n_________|______________|____________");
+
+	for (i = OTPMK_SIZE_BYTES - 4; i >= 0; i = i - 4) {
+		printf("\nOTPMKR %d | %3d-%3d\t|   %.2x%.2x%.2x%.2x ",
+		       OTPMK_REG_NO - 1 - i / 4,
+		       OTPMK_SIZE_BYTES - 1 + (OTPMK_REG_NO - 1 -
+					       i / 4) * OTPMK_SIZE_BYTES,
+		       (OTPMK_REG_NO - 1 - i / 4) * OTPMK_SIZE_BYTES,
+		       otpmk_hex[j], otpmk_hex[j + 1], otpmk_hex[j + 2],
+		       otpmk_hex[j + 3]);
+		j = j + 4;
 	}
 
 }
@@ -102,7 +132,12 @@ void generate_code_bits(bool number[])
 		k[8] = '\0';
 		otpmk_hex[i] = strtoul(k, NULL, 2);
 	}
-	print_otpmk();
+
+	if (trust_arch == 1)
+		print_otpmk_trust1();
+
+	if (trust_arch == 2)
+		print_otpmk_trust2();
 }
 
 void gen_rand_string()
@@ -110,7 +145,7 @@ void gen_rand_string()
 	unsigned int iseed = (unsigned int)time(NULL);
 	int i, l, index1, index2;
 	char hex_digits[] = "0123456789abcdef";
-	char rand_string[2];
+	char rand_string[3] = {'\0'};
 
 	/*providing seed for random number generation*/
 	srand(iseed);
@@ -150,9 +185,9 @@ int check_string(char *str)
 
 void usage()
 {
-	printf("\nUsage: ./gen_otpmk [string]\n");
+	printf("\nUsage: ./gen_otpmk <trust_arch> [string]\n");
 	printf("string : 32 byte string\n");
-	printf("e.g. gen_otpmk 11111111222222223333333344444444"
+	printf("e.g. gen_otpmk 1 11111111222222223333333344444444"
 			"55555555666666667777777788888888\n");
 }
 
@@ -161,25 +196,21 @@ int main(int argc, char *argv[])
 	bool num[OTPMK_SIZE_BITS];
 	char otpmk_in[2];
 	int i, j, l, ret;
-	if (argc == 2) {
-		if ((strcmp(argv[1], "--help") == 0)
-			|| (strcmp(argv[1], "-h") == 0)) {
-			usage();
-			exit(0);
-		}
+	if (argc == 3 &&
+	    (strlen(argv[1]) == 1 && (*argv[1] == '1' || *argv[1] == '2'))) {
+		trust_arch = atoi(argv[1]);
 		/*check length of hexadecimal string*/
-		if (strlen(argv[1]) == 2 * OTPMK_SIZE_BYTES) {
+		if (strlen(argv[2]) == 2 * OTPMK_SIZE_BYTES) {
 			/*check if string is valid hexadecimal string*/
-			ret = check_string(argv[1]);
+			ret = check_string(argv[2]);
 			if (ret == -1) {
-				printf
-				    ("\nError: Input key is not having "
+				printf("\nError: Input key is not having"
 					"valid hexadecimal character\n");
 				return -1;
 			}
-			for (i = 0; i < 2 * OTPMK_SIZE_BYTES; i = i + 2) {
-				otpmk_in[0] = argv[1][i + 0];
-				otpmk_in[1] = argv[1][i + 1];
+			for (i = 0; i < 2 * OTPMK_SIZE_BYTES; i += 2) {
+				otpmk_in[0] = argv[2][i + 0];
+				otpmk_in[1] = argv[2][i + 1];
 				l = i / 2;
 				otpmk_hex[l] = strtoul(otpmk_in, NULL, 16);
 			}
@@ -187,12 +218,23 @@ int main(int argc, char *argv[])
 			printf("\nError: Invalid Input key Length\n");
 			usage();
 			exit(1);
-
 		}
-	} else if (argc == 1) {
-		printf("\nGenerating random key as input "
+	} else if (argc == 2) {
+		if ((strcmp(argv[1], "--help") == 0)
+			|| (strcmp(argv[1], "-h") == 0)) {
+			usage();
+			exit(0);
+		} else if (strlen(argv[1]) == 1 &&
+			   (*argv[1] == '1' || *argv[1] == '2')) {
+			printf("\nGenerating random key as input "
 				"string not provided\n");
-		gen_rand_string();
+			trust_arch = *argv[1] - 48;
+			gen_rand_string();
+		} else {
+			printf("\nError: Wrong Usage\n");
+			usage();
+			exit(1);
+		}
 	} else {
 		printf("\nError: Wrong Usage\n");
 		usage();
