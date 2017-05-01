d429453c85b386d0e158305679cf9144ed158417
Udit Agarwal <udit.agarwal@nxp.com>
2017-05-01 10:54:09 +0530

    LS1088A: Signing PPA image using script
    Adds the ./uni_sign command in the script to automatically
    generate header for PPA image (QSPI boot).
    
    Signed-off-by: Udit Agarwal <udit.agarwal@nxp.com>

--- scripts/platforms/ls2088_1088_qspi.sh
+++ scripts/platforms/ls2088_1088_qspi.sh
@@ -40,6 +40,9 @@
 # Sign bootscript image
 ./uni_sign input_files/uni_sign/ls2088_1088/qspi/input_bootscript_secure
 
+# Sign PPA image
+./uni_sign input_files/uni_sign/ls2088_1088/qspi/input_ppa_secure
+
 # Sign mc image
 ./uni_sign input_files/uni_sign/ls2088_1088/qspi/input_mc_secure
 
@@ -62,6 +65,7 @@ fi
 touch secboot_hdrs.bin
 dd if=bootscript of=secboot_hdrs.bin bs=1K seek=0
 dd if=hdr_bs.out of=secboot_hdrs.bin bs=1K seek=256
+dd if=hdr_ppa.out of=secboot_hdrs.bin bs=1K seek=512
 dd if=hdr_uboot.out of=secboot_hdrs.bin bs=1K seek=768
 dd if=hdr_mc.out of=secboot_hdrs.bin bs=1K seek=1024
 dd if=hdr_dpc.out of=secboot_hdrs.bin bs=1K seek=1280
