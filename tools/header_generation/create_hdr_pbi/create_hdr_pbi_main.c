6d83dd80bc691a55f09590bddc7c58c8f5a27bc6
Udit Agarwal <udit.agarwal@nxp.com>
2018-12-20 18:31:03 +0530

    Removes "All Rights Reserved" Text.
    "All Rights Reserved" text removed adjacent to copyright
    message as per legal guidance.
    
    Signed-off-by: Udit Agarwal <udit.agarwal@nxp.com>

--- tools/header_generation/create_hdr_pbi/create_hdr_pbi_main.c
+++ tools/header_generation/create_hdr_pbi/create_hdr_pbi_main.c
@@ -1,6 +1,5 @@
 /* Copyright (c) 2015 Freescale Semiconductor, Inc.
  * Copyright 2018 NXP
- * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
